4e9ba6ddff07190d2324b72e7588bb90b6261f90
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-07-23 00:26:06 -0500

    Added the routes for the project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navbar from './nav';
+import Companies from './companies';
+import CompanyDetail from './company';
+import Jobs from "./jobs";
+import JobDetail from './job';
+import "./index.css"
+
+const App = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Route exact path="/" >
+      {/* <Home/> */}
+    </Route>
+    <Route exact path="/companies" >
+      <Companies />
+    </Route>
+    <Route exact path="/companies/:name" >
+      <CompanyDetail />
+    </Route>
+    <Route exact path="/jobs" >
+      <Jobs />
+    </Route>
+    <Route exact path="/jobs/:name" >
+      <JobDetail />
+    </Route>
+    <Route exact path="/signup" >
+      {/* <Signup/> */}
+    </Route>
+    <Route exact path="/login" >
+      {/* <Login/> */}
+    </Route>
+    <Route exact path="/profile" >
+      {/* <Profile/> */}
+    </Route>
+  </BrowserRouter>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
